c17e01e0d8b195791cf42d02436410e51302e670
Eduardo <e.chiaro@gmail.com>
2018-08-04 10:36:59 -0700

    fixing vunerability

--- gulpfile.js
+++ gulpfile.js
@@ -24,9 +24,6 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
-});
 
 gulp.task('css', function () {
     var processors = [
@@ -46,11 +43,15 @@ gulp.task('css', function () {
         .pipe(livereload());
 });
 
+gulp.task('build', gulp.series('css', function() {
+    return nodemonServerInit();
+}));
+
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
-gulp.task('zip', ['css'], function () {
+gulp.task('zip', gulp.series('css', function() {
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
@@ -62,8 +63,8 @@ gulp.task('zip', ['css'], function () {
     ])
         .pipe(zip(filename))
         .pipe(gulp.dest(targetDir));
-});
+}));
 
-gulp.task('default', ['build'], function () {
+gulp.task('default', gulp.series('build', function() {
     gulp.start('watch');
-});
+}));
